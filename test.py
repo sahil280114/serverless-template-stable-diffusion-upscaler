ebbb87feb50bde2609241bbc46b61e7203b28226
sahil280114 <sahil280114@gmail.com>
2023-01-18 13:46:00 +0100

    update requirements

--- test.py
+++ test.py
@@ -5,13 +5,17 @@ import requests
 import base64
 from io import BytesIO
 from PIL import Image
+import banana_dev as banana
+model_inputs = {'prompt': 'a white cat','image_url':'https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd2-upscale/low_res_cat.png'}
 
-model_inputs = {'prompt': '','image_url':''}
-
+#call on a local running gpu
 res = requests.post('http://localhost:8000/', json = model_inputs)
 
 image_byte_string = res.json()["image_base64"]
 
+#call on banana
+out = banana.run("api_key","model_key",model_inputs)
+image_byte_string = out["modelOutputs"][0]["image_base64"]
 image_encoded = image_byte_string.encode('utf-8')
 image_bytes = BytesIO(base64.b64decode(image_encoded))
 image = Image.open(image_bytes)
