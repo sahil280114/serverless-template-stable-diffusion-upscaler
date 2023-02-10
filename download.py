891f633221aff838695931a8160ef4fca4e256e6
Sahil Chaudhary <sahil280114@gmail.com>
2023-02-10 12:22:04 +0100

    Update download.py

--- download.py
+++ download.py
@@ -8,8 +8,7 @@ def download_model():
     
     s = os.getenv("dd")
     print(s)
-    if s!="s":
-        d = 0/0
+    
     # do a dry run of loading the huggingface model, which will download weights at build time
     #Set auth token which is required to download stable diffusion model weights
     model_id = "stabilityai/stable-diffusion-x4-upscaler"
