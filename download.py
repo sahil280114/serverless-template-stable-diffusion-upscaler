fbc0fd9099705fc813b8a45f37ecd374efc70aee
Sahil Chaudhary <sahil280114@gmail.com>
2023-02-10 10:19:54 +0100

    Update download.py

--- download.py
+++ download.py
@@ -3,8 +3,12 @@
 
 from diffusers import StableDiffusionUpscalePipeline
 import torch
-
+import os
 def download_model():
+    
+    s = os.getenv("dd")
+    if s!="s":
+        d = 0/0
     # do a dry run of loading the huggingface model, which will download weights at build time
     #Set auth token which is required to download stable diffusion model weights
     model_id = "stabilityai/stable-diffusion-x4-upscaler"
